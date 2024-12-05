af3f2c9042347e527caee8fc29a83a85c3217b2f
Dusan Tomic <duletomic14901@gmail.com>
2024-12-05 15:54:52 +0100

    changes in app.js

--- MealsToGo/App.js
+++ MealsToGo/App.js
@@ -1,20 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Platform, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.top}>
+        <Text>Search</Text>
+      </View>
+      <View style={styles.bottom}>
+        <Text>sth</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Platform.OS === 'android' ? 35: 0,
   },
+  top: {
+    backgroundColor: 'green',
+    padding: 30,
+  },
+  bottom: {
+    flex: 1,
+    backgroundColor: 'blue',
+  }
 });
